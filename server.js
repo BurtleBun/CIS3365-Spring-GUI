1583b5a16ff9c10d57d1a0175b646edd3e5afbd2
Sc0rpinok <112124620+Sc0rpinok@users.noreply.github.com>
2023-04-19 20:56:48 -0500

    Update server.js
    Added template server.js

--- server.js
+++ server.js
@@ -5,25 +5,149 @@ const ejs = require('ejs');
 const app = express();
 const bodyParser = require('body-parser');
 
-var path = require('path');
+// Enable CORS
+// Cross Origin Resource Sharing used for the event listeners
+// due to same origin policy, we set this so it can contact the backend python
+// and the database
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    // respond with 200
+    res.sendStatus(200);
+  } else {
+    // move on
+    next();
+  }
+});
 
-var routes = require('./routes/index');
+// Enable JSON body parsing for incoming requests
+app.use(express.json());
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Set the views directory to the public directory
+app.set('views', __dirname + '/public/views');
 
-// Use application-level middleware for common functionality, including logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/views')));
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define the API endpoint
+const captainAPI = 'http://127.0.0.1:5000/captain';
+const homeAPI = 'http://127.0.0.1:5000/enroute';
+const toggledHomeAPI = 'http://127.0.0.1:5000/arrived'
+const loginAPI = 'http://127.0.0.1:5000/';
+const shipAPI = 'http://127.0.0.1:5000/ship';
+const cargoAPI = 'http://127.0.0.1:5000/cargo'
+
+// Login Page
+app.get('/', (req, res) => {
+  res.render('pages/login.ejs');
+});
+
+app.post('/signin', function(req, res) {
+  axios.get(loginAPI)
+  .then(response => {
+    // Get masterLogin from the backend server
+    const masterUser = response.data.username;
+    const masterPass = response.data.password
+    console.log('Master Username:', masterUser);  //Debug statement
+    console.log('Master Password:', masterPass);  //Debug statement
+
+    // Get inputted Username and Password
+    var inputUsername = req.body.username
+    var inputPassword = req.body.password
+    console.log('Inputted Username:', inputUsername); //Debug statement
+    console.log('Inputted Password:', inputPassword); //Debug statement
+
+    let confirmLogin = 0; // 0 is false for incorrect login
+    if (inputUsername == masterUser && inputPassword == masterPass) {
+        confirmLogin = 1; // 1 is true for correct login
+        // Render the overview page with scheduled cargo
+        res.redirect('/overview');
+      }
+    else
+      res.render('pages/login.ejs', {checkLogin: confirmLogin})
+  });
+});
 
 
-app.use('/', routes);
+// Home Page (Shows cargo in transit)
+app.get('/overview', (req, res) => {
+  axios.get(homeAPI)
+    .then((response) => {
+      const scheduledCargo = response.data;
+      res.render('pages/overview.ejs', {cargo: scheduledCargo});
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).send('Error fetching cargo data');
+    })
+  
+});
 
-const port_runing = 3000;
+// Toggled Home Page (Shows arrived cargo)
+app.get('/toggledOverview', (req, res) => {
+  axios.get(toggledHomeAPI)
+    .then((response) => {
+      const scheduledCargo = response.data;
+      res.render('pages/toggledOverview.ejs', {cargo: scheduledCargo});
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).send('Error fetching cargo data');
+    })
+  
+});
 
-app.listen(port_runing);
+// Render the captains.ejs file with the captains data
+app.get('/captain', (req, res) => {
+  // Use axios to fetch the captain data
+  axios.get(captainAPI)
+    .then((response) => {
+      const captains = response.data;
+      res.render('pages/captains.ejs', {captains: captains});
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error fetching captain data');
+    });
+});
 
-console.log("Application running at: " + port_runing)
+// Render the ships.ejs file with the spaceship data
+app.get('/ship', (req, res) => {
+  // Use axios to fetch the captain data
+  axios.get(shipAPI)
+    .then((response) => {
+      const ships = response.data;
+      res.render('pages/ships.ejs', {ships: ships});
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error fetching spaceship data');
+    });
+});
+
+// Render the cargo.ejs file with the cargo data
+// This is just the overview page but showing every single cargo to be editable
+app.get('/cargo', (req, res) => {
+  // Use axios to fetch the captain data
+  axios.get(cargoAPI)
+    .then((response) => {
+      const cargo = response.data;
+      res.render('pages/cargo.ejs', {cargo: cargo});
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error fetching cargo data');
+    });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
